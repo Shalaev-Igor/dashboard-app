c8ccbfbccdece4b6747f5189118fb58b8a0ffa45
Igor <jiukcmeptu@gmail.com>
2023-01-23 11:29:58 +0200

    Add Store from Redux

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,10 +1,13 @@
-import data from '../mock/data.json';
+import { useSelector } from 'react-redux';
 import { JobPosition } from './JobPosition';
+import { selectAllPosition } from 'store/positions/position-selector';
 
 const JobList = () => {
+  const positions = useSelector(selectAllPosition);
+
   return (
     <div className='job-list'>
-      {data.map(item => (
+      {positions.map(item => (
         <JobPosition key={item.id} {...item} />
       ))}
     </div>
